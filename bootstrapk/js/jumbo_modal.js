fb185bbf8e38be39356e11b34b519fec2e231b70
inae95 <67131985+inae95@users.noreply.github.com>
2020-11-11 15:13:17 +0900

    Add files via upload

---
+++ bootstrapk/js/jumbo_modal.js
@@ -0,0 +1,13 @@
+$('#jumbo_btn').css({
+  'border':'1px solid white',
+  'border-radius':'10px',
+  'padding':'20px'
+});
+
+
+// $('.section03 .img_box>img').removeClass('btn')
+// var btnOpen=$('section03 .img_box>img');
+// btnOpen.removeClass('btn').removeAttr('data-toggle').removeAttr{'data-taget'};
+// btnOpen.click(function(){
+//   $(this).next().modal('show')
+// })
